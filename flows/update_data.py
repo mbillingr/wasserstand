61dfdb6f6a7bf5e8621417da64d81794f9af0a46
Martin Billinger <flkazemakase@gmail.com>
2021-10-08 20:04:53 +0200

    prefect flow to collect data

--- flows/update_data.py
+++ flows/update_data.py
@@ -1,17 +1,53 @@
 import pandas as pd
-import urllib.request
+from prefect import Flow, task
 
-URL = "http://wiski.tirol.gv.at/hydro/ogd/OGD_W.csv".
-df = pd.read_csv(URL, encoding="ISO-8859-1")
-print(df)
+URL = "https://wiski.tirol.gv.at/hydro/ogd/OGD_W.csv"
+
+DATAFILE_TEMPLATE = "../data/wasser_%Y-%m-%d.parquet"
 
 
+@task
 def fetch_level_data(url):
     df = pd.read_csv(url, encoding="ISO-8859-1", sep=';', parse_dates=['Zeitstempel in ISO8601'])
     df['timestamp_utc'] = df['Zeitstempel in ISO8601'].apply(lambda x: x.tz_convert('UTC')).values.astype('datetime64')
     df['date'] = df['timestamp_utc'].astype('datetime64[D]')
+    df = df.drop(columns='Zeitstempel in ISO8601')
     return df
 
 
-level_data = fetch_level_data(URL)
-print(level_data['date'])
+@task(nout=2)
+def split_days(df):
+    groups = list(df.groupby("date"))
+    dates = [g[0] for g in groups]
+    dfs = [g[1] for g in groups]
+    return dates, dfs
+
+
+@task
+def update_daydata(date, new_df):
+    new_df = new_df.reset_index(drop=True)
+
+    try:
+        df = load_day(date)
+        df = pd.concat([df, new_df]).drop_duplicates()
+    except FileNotFoundError:
+        df = new_df
+    df = df.sort_values(['Stationsnummer', 'timestamp_utc'])
+    store_day(date, df)
+
+
+def load_day(date):
+    return pd.read_parquet(date.strftime(DATAFILE_TEMPLATE))
+
+
+def store_day(date, df):
+    df.to_parquet(date.strftime(DATAFILE_TEMPLATE), index=False)
+    df.to_csv(date.strftime(DATAFILE_TEMPLATE) + '.csv', index=False)
+
+
+with Flow("fetch-water-data") as flow:
+    level_data = fetch_level_data(URL)
+    dates, levels = split_days(level_data)
+    update_daydata.map(dates, levels)
+
+flow.run()
