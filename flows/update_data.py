704c1b494fa8d726bf56daeb22447b35e9b78746
anon <n/a>
2021-10-11 17:18:41 +0200

    downsample and interpolate data

--- flows/update_data.py
+++ flows/update_data.py
@@ -1,10 +1,23 @@
 import pandas as pd
 import prefect
 from prefect import Flow, task
+import numpy as np
 
-from config import DATA_SOURCE_URL, DATAFILE_TEMPLATE, DATAFILE_LATEST
+from config import (
+    DATA_SOURCE_URL,
+    DATAFILE_TEMPLATE,
+    DATAFILE_LATEST,
+    SAMPLE_INTERVAL_MINUTES,
+    RESAMPLE_TO_MINUTES,
+)
 
 SOURCE_TIMESTAMP_COLUMN = "Zeitstempel in ISO8601"
+VALUE_COLUMN = "Wert"
+STATION_COLUMN = "Stationsnummer"
+TIME_COLUMN = "timestamp_utc"
+DATE_COLUMN = "date"
+
+BAD_VALUE_INDICATOR = -777.0
 
 
 @task
@@ -16,14 +29,69 @@ def fetch_latest_level_data(url):
 
 @task
 def convert_timestamp(df):
-    df["timestamp_utc"] = (
-        df[SOURCE_TIMESTAMP_COLUMN]
-        .apply(lambda x: x.tz_convert("UTC"))
-        .values.astype("datetime64")
+    df = df.assign(
+        timestamp_utc=(
+            df[SOURCE_TIMESTAMP_COLUMN]
+            .apply(lambda x: x.tz_convert("UTC"))
+            .values.astype("datetime64")
+        )
     )
-    df["date"] = df["timestamp_utc"].astype("datetime64[D]")
+    df = df.assign(date=df["timestamp_utc"].astype("datetime64[D]"))
     df = df.drop(columns=SOURCE_TIMESTAMP_COLUMN)
     return df
+
+
+@task
+def remove_bad_rows(df):
+    n_before = len(df)
+    df = df[df[VALUE_COLUMN] != BAD_VALUE_INDICATOR]
+    if len(df) != n_before:
+        logger = prefect.context.get("logger")
+        logger.info(f"Removed {n_before - len(df)} bad rows")
+    return df
+
+
+@task
+def interpolate_missing(df):
+    start_time = df["timestamp_utc"].min()
+    end_time = df["timestamp_utc"].max()
+    step = np.timedelta64(SAMPLE_INTERVAL_MINUTES, "m")
+    expected_times = set(
+        np.arange(start_time, end_time + step, step).astype("datetime64[m]")
+    )
+
+    stations = df[STATION_COLUMN].unique()
+
+    missing = []
+
+    for s in stations:
+        station_data = df[df[STATION_COLUMN] == s]
+        times = set(station_data[TIME_COLUMN].values.astype("datetime64[m]"))
+        for t in expected_times - times:
+            new_row = station_data.iloc[:1].copy()
+            new_row[VALUE_COLUMN] = np.nan
+            new_row[TIME_COLUMN] = t
+            new_row[DATE_COLUMN] = np.datetime64(t, "D")
+            assert not np.isnan(
+                station_data[VALUE_COLUMN].iloc[0]
+            )  # make sure we did not overwrite original data
+            missing.append(new_row)
+
+    df = pd.concat([df] + missing)
+    df = df.sort_values(["Stationsnummer", "timestamp_utc"])
+
+    df[VALUE_COLUMN] = df.groupby(STATION_COLUMN).apply(
+        lambda x: x[[VALUE_COLUMN]].interpolate()
+    )
+
+    return df
+
+
+@task
+def downsample(df):
+    minutes = df["timestamp_utc"].apply(lambda t: t.minute)
+    mask = minutes.apply(lambda m: m in RESAMPLE_TO_MINUTES)
+    return df[mask]
 
 
 @task(nout=2)
@@ -68,7 +136,10 @@ def load_data(url):
 
 with Flow("fetch-water-data") as flow:
     level_data = fetch_latest_level_data(DATA_SOURCE_URL)
+    level_data = remove_bad_rows(level_data)
     level_data = convert_timestamp(level_data)
+    level_data = downsample(level_data)
+    level_data = interpolate_missing(level_data)
     store_latest_data(level_data)
 
     dates, levels = split_days(level_data)
--- flows/update_existing.py
+++ flows/update_existing.py
@@ -1,12 +1,16 @@
+import numpy as np
 import pandas as pd
 import prefect
 from prefect import Flow, task
 import s3fs
 
-from config import DATA_ROOT
+from config import DATA_ROOT, RESAMPLE_TO_MINUTES, SAMPLE_INTERVAL_MINUTES
 
 SOURCE_TIMESTAMP_COLUMN = "Zeitstempel in ISO8601"
 VALUE_COLUMN = "Wert"
+STATION_COLUMN = "Stationsnummer"
+TIME_COLUMN = "timestamp_utc"
+DATE_COLUMN = "date"
 
 BAD_VALUE_INDICATOR = -777.0
 
@@ -30,6 +34,7 @@ def load_data(file):
 @task
 def backup(file, df):
     df.to_parquet("../data/backup/" + file, index=False)
+    df.to_csv("../data/backup/" + file + ".csv", index=False)
 
 
 @task
@@ -42,11 +47,56 @@ def remove_bad_rows(df):
     return df
 
 
+@task
+def interpolate_missing(df):
+    start_time = df["timestamp_utc"].min()
+    end_time = df["timestamp_utc"].max()
+    step = np.timedelta64(SAMPLE_INTERVAL_MINUTES, "m")
+    expected_times = set(
+        np.arange(start_time, end_time + step, step).astype("datetime64[m]")
+    )
+
+    stations = df[STATION_COLUMN].unique()
+
+    missing = []
+
+    for s in stations:
+        station_data = df[df[STATION_COLUMN] == s]
+        times = set(station_data[TIME_COLUMN].values.astype("datetime64[m]"))
+        for t in expected_times - times:
+            new_row = station_data.iloc[:1].copy()
+            new_row[VALUE_COLUMN] = np.nan
+            new_row[TIME_COLUMN] = t
+            new_row[DATE_COLUMN] = np.datetime64(t, "D")
+            assert not np.isnan(
+                station_data[VALUE_COLUMN].iloc[0]
+            )  # make sure we did not overwrite original data
+            missing.append(new_row)
+
+    df = pd.concat([df] + missing)
+    df = df.sort_values(["Stationsnummer", "timestamp_utc"])
+
+    df[VALUE_COLUMN] = df.groupby(STATION_COLUMN).apply(
+        lambda x: x[[VALUE_COLUMN]].interpolate()
+    )
+
+    return df
+
+
+@task
+def downsample(df):
+    minutes = df["timestamp_utc"].apply(lambda t: t.minute)
+    mask = minutes.apply(lambda m: m in RESAMPLE_TO_MINUTES)
+    return df[mask]
+
+
 with Flow("fetch-water-data") as flow:
     files = find_data_files()
     data = load_data.map(files)
     backup.map(files, data)
+    data = downsample.map(data)
     data = remove_bad_rows.map(data)
+    data = interpolate_missing.map(data)
     store_data.map(files, data)
 
 
