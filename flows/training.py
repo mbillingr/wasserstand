808d434016738c81f9e14d099a2374cf5702e57c
Martin Billinger <mbillingr@users.noreply.github.com>
2021-10-11 23:03:16 +0200

    crude constant prediction

--- flows/training.py
+++ flows/training.py
@@ -13,44 +13,67 @@ def load_data(source=DATAFILE_ALL):
 
 
 @task
-def extract_features(dataframe):
-    stations = dataframe["Stationsnummer"].unique().compute()
+def extract_time_series(dataframe):
+    stations = {id: name for _, (id, name) in dataframe[["Stationsnummer", "Stationsname"]].compute().iterrows()}
 
     station_series = []
     all_times = []
     for s in stations:
         sdf = dataframe[dataframe["Stationsnummer"] == s]
-        x = sdf.compute()
-        x.to_csv(f"../data/{s}.csv")
+        #sdf.compute().to_csv(f"../data/{stations[s]}.csv")
 
         times = sdf["timestamp_utc"].values
         series = sdf["Wert"].values
         station_series.append(series)
 
-        print(s, len(times.compute()))  # , len(da.unique(times)))
-
-        # print(da.all(times.values == da.unique(times.values)).compute())
-
         all_times.append(times)
 
-        # print(times.min().compute(), times.max().compute(), len(times))#, ((times.max() - times.min()) / len(times).compute()))
+    times = times.compute()
 
     time_series = da.concatenate(
-        [station_series], axis=1, allow_unknown_chunksizes=True
-    )
-    print(time_series.compute())
+        [station_series], allow_unknown_chunksizes=True
+    ).T
+
+    idx = list(stations.values()).index('Innsbruck')
+
+    err = estimate_prediction_error(PredictConstant(), 10, time_series)
+    s = da.std(err, axis=0)[:, idx].compute()
+
+    pred = PredictConstant().predict(10, time_series).compute()
+
+    plt.plot(time_series[:, idx])
+    plt.plot(np.arange(10)+time_series.shape[0], pred[:, idx], '--')
+    plt.fill_between(np.arange(10)+time_series.shape[0], pred[:, idx]+s, pred[:, idx]-s, alpha=0.3)
+    plt.show()
 
 
 class PredictConstant:
     def __init__(self):
         pass
 
-    def predict(self, features):
-        pass
+    def predict_next(self, time_series):
+        time_series.compute_chunk_sizes()
+        return time_series[-1:, :]
+
+    def predict(self, n, time_series):
+        for _ in range(n):
+            time_series = da.concatenate([time_series, self.predict_next(time_series)], axis=0)
+        return time_series[-n:, :]
+
+
+def estimate_prediction_error(model, n, time_series):
+    time_series.compute_chunk_sizes()
+    all_errors = []
+    for offset in [10, 20, 30, 40, 50, 60, 70]:
+        known = time_series[:offset, :]
+        predictions = model.predict(n, known)
+        errors = predictions - time_series[offset:offset+n]
+        all_errors.append(errors)
+    return da.stack(all_errors)
 
 
 with Flow("training") as flow:
     data = load_data()
-    extract_features(data)
+    extract_time_series(data)
 
 flow.run()
