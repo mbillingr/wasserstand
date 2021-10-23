3ad9bf0b9790fcd85f31f023c165532d1f83b400
Martin Billinger <flkazemakase@gmail.com>
2021-10-23 13:23:46 +0200

    prefect agent container service

--- infrastructure/aws/app.py
+++ infrastructure/aws/app.py
@@ -15,7 +15,9 @@ class DatascienceStack(core.Stack):
             id="TASKROLE",
             assumed_by=iam.ServicePrincipal(service="ecs-tasks.amazonaws.com"),
         )
-        prefect_agent_role.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name('AmazonECS_FullAccess'))
+        prefect_agent_role.add_managed_policy(
+            iam.ManagedPolicy.from_aws_managed_policy_name("AmazonECS_FullAccess")
+        )
 
         public_subnet = ec2.SubnetConfiguration(
             name="Public", subnet_type=ec2.SubnetType.PUBLIC, cidr_mask=28
@@ -41,21 +43,29 @@ class DatascienceStack(core.Stack):
             scope=self, id="CLUSTER", cluster_name=cluster_name, vpc=vpc
         )
 
-        task_definition = ecs.FargateTaskDefinition(
+        agent_task_definition = ecs.FargateTaskDefinition(
             scope=self,
             id="PrefectAgent",
             task_role=prefect_agent_role,
         )
 
-        container = task_definition.add_container(
-            id='PrefectAgent',
-            image=ecs.ContainerImage.from_registry(name='prefecthq/prefect:latest'),
+        agent_container = agent_task_definition.add_container(
+            id="PrefectAgent",
+            image=ecs.ContainerImage.from_registry(name="prefecthq/prefect:latest"),
             environment={
-                'PREFECT__CLOUD__AGENT__LABELS': "[]",
-                'PREFECT__CLOUD__AGENT__LEVEL': 'INFO',
-                'PREFECT__CLOUD__API': 'https://api.prefect.io',
-                'PREFECT__CLOUD__API_KEY': get_secret('PREFECT__CLOUD__API_KEY'),
+                "PREFECT__CLOUD__AGENT__LABELS": "['wasserstand']",
+                "PREFECT__CLOUD__AGENT__LEVEL": "INFO",
+                "PREFECT__CLOUD__API": "https://api.prefect.io",
+                "PREFECT__CLOUD__API_KEY": get_secret("PREFECT__CLOUD__API_KEY"),
             },
+            command=["prefect", "agent", "ecs", "start", "--cluster", cluster_name],
+        )
+
+        agent_service = ecs.FargateService(
+            self,
+            "PrefectAgentService",
+            task_definition=agent_task_definition,
+            cluster=cluster,
         )
 
 
